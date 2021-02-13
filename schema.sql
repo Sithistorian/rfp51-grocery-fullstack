9bb30f4f37fdd367086b2831ea1a85d30d7c0d23
Tristan <tristanshawnjohnson@gmail.com>
2021-02-13 10:11:31 -0800

    Got a get request working and the database connected

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
 /*  Execute this file from the command line by typing:
  *    sqlite3 items.db < schema.sql
  *  to create the database and the tables.*/
+
+ CREATE DATABASE IF NOT EXISTS groceryList;
+
+ USE groceryList;
--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const app = express();
 const port = 8080;
 const cors = require("cors");
 const db = require("../db/index.js");
-
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('This is working')
+})
+
 
 app.listen(port, () => {
   console.log("listening on 8080");
